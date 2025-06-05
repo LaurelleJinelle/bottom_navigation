c1813832966b8931b7525b964188917e9e1461b5
LaurelleJinelle <j.nformi1@alustudent.com>
2025-06-05 12:48:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'screens/first_screen.dart';
+import 'screens/second_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+      ),
+      home: MyHomePage(), // Updated to use bottom navigation
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // Add the two screens you want to switch between
+  final List<Widget> _pages = [FirstScreen(), SecondScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.purple,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'First'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.screen_share),
+            label: 'Second',
+          ),
+        ],
+      ),
+    );
+  }
+}
